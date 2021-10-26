5a651c958099f8fa78ecdc193e7bf987a7196e7c
jnhro1 <jnhro1@gmail.com>
2021-10-26 23:23:32 +0900

    #13 서버 api path 동기화

--- routes/user.js
+++ routes/user.js
@@ -8,18 +8,18 @@ require("dotenv").config();
 
 router.post("/join", Validator.join, ValidatorError.join, userCtrl.join);
 router.post("/login", Validator.login, ValidatorError.login, userCtrl.login);
-router.post(
-    "/emailVerify",
+router.put(
+    "/email-verify",
     Validator.emailVerify,
     ValidatorError.emailVerify,
     userCtrl.emailVerify
 );
 router.post(
-    "/reissueAccessToken",
+    "/reissue-access-token",
     auth.verifyRefreshToken,
     userCtrl.reissueAccessToken
 );
-router.get("/logout", auth.verifyToken, userCtrl.logout);
+router.post("/logout", auth.verifyToken, userCtrl.logout);
 router.get("/auth", auth.verifyToken, userCtrl.auth);
 
 // accessToken, refeshToken 재발급 과정 동작이 원활한지 테스트를 만들도록 함
