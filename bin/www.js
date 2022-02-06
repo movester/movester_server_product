1ab45fd95288d31b086bc9fa18cec18cdbac3174
jnhro1 <jnhro1@gmail.com>
2022-02-07 00:01:39 +0900

    #16 Fix: index.js www.js 분리

--- bin/www.js
+++ bin/www.js
@@ -1,22 +1,6 @@
-const express = require('express');
-
-const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const router = require('../routes/index');
+const app = require('..');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/', router);
-
-app.listen(port, () => console.log(`Prod Server listening on port ${port}!`));
+app.listen(port, () => console.log(`Dev Server listening on port ${port}!`));
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const router = require('./routes');
+require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api', router);
+
+module.exports = app;
