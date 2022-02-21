d96fd06980f279ad9bd61fb6fcf829567a962f6c
jnhro1 <jnhro1@gmail.com>
2022-02-21 21:39:13 +0900

    #27 Feat: 동일한 비밀번호로 변경 요청시 실패

--- controllers/user.js
+++ controllers/user.js
@@ -161,6 +161,9 @@ const changePassword = async (req, res) => {
     const isCorrectPassword = await encrypt.compare(curPassword, user.password);
     if (!isCorrectPassword) return res.status(CODE.BAD_REQUEST).json(form.fail(MSG.PW_MISMATCH));
 
+    const isSamePassword = await encrypt.compare(newPassword, user.password);
+    if (isSamePassword) return res.status(CODE.BAD_REQUEST).json(form.fail("기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다."));
+
     await userService.resetPassword(userIdx, newPassword);
 
     return res.status(CODE.OK).json(form.success());
