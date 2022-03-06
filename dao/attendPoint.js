f79f57337a04886c633ae3353e98d52188b99e9a
jnhro1 <jnhro1@gmail.com>
2022-03-06 22:13:10 +0900

    #60 Fix: getAttendPointsByYearMonth 병합시 누락된 코드 추가

--- dao/attendPoint.js
+++ dao/attendPoint.js
@@ -6,8 +6,7 @@ const createAttendPoint = async (userIdx, year, month) => {
     connection = await pool.getConnection(async conn => conn);
 
     const sql = `INSERT
-                   INTO attend_point (user_idx, attend_year, attend_month)
-                 VALUES (${userIdx}, ${year}, ${month});`;
+                 INTO attend_point (user_idx, attend_year, attend_month) VALUES (${userIdx}, ${year}, ${month});`;
 
     const [row] = await connection.query(sql);
     return !!Object.keys(row);
@@ -19,21 +18,45 @@ const createAttendPoint = async (userIdx, year, month) => {
   }
 };
 
-const findAttendPointByDate = async (userIdx, year, month, date) => {
+const findRecentAttendPoint = async userIdx => {
   let connection;
 
   try {
     connection = await pool.getConnection(async conn => conn);
 
-    const sql = `SELECT user_idx, create_at
+    const sql = `SELECT DATE_FORMAT(create_at,'%Y %m %d') AS date
                    FROM attend_point
                   WHERE user_idx = ${userIdx}
-                    AND DATE(create_at) = '${year}-${month}-${date}';`;
+               ORDER BY create_at DESC
+                  LIMIT 1;`;
 
     const [row] = await connection.query(sql);
-    return !!row.length;
+    return row[0]?.date;
   } catch (err) {
-    console.error(`=== AttendPoint Dao findAttendPointByDate Error: ${err} === `);
+    console.error(`=== AttendPoint Dao createAttendPoint Error: ${err} === `);
+    throw new Error(err);
+  } finally {
+    connection.release();
+  }
+};
+
+const getAttendPointsByYearMonth = async (userIdx, year, month) => {
+  let connection;
+
+  try {
+    connection = await pool.getConnection(async conn => conn);
+
+    const sql = `SELECT DATE_FORMAT(create_at,'%e') AS day
+                   FROM movester_db.attend_point
+                  WHERE user_idx = ${userIdx}
+                    AND attend_year = ${year}
+                    AND attend_month = ${month}
+               ORDER BY attend_point_idx;`;
+
+    const [row] = await connection.query(sql);
+    return row;
+  } catch (err) {
+    console.error(`=== AttendPoint Dao getAttendPointsByYearMonth Error: ${err} === `);
     throw new Error(err);
   } finally {
     connection.release();
@@ -42,5 +65,6 @@ const findAttendPointByDate = async (userIdx, year, month, date) => {
 
 module.exports = {
   createAttendPoint,
-  findAttendPointByDate,
+  findRecentAttendPoint,
+  getAttendPointsByYearMonth,
 };
